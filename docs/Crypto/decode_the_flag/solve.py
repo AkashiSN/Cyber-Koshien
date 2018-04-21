eac8363883364bf674e1cadfa2d90d1cd7dceae2
Akashi_SN <btorntireinvynriy@gmail.com>
2018-04-21 18:51:49 +0900

    Update

--- docs/Crypto/decode_the_flag/solve.py
+++ docs/Crypto/decode_the_flag/solve.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
-import commands,re
+#!/usr/bin/env python3
+import subprocess
 
-enc = open("crypto.txt","r")
+Enc = "aes-128-cbc aes-128-ecb aes-192-cbc aes-192-ecb aes-256-cbc aes-256-ecb base64 bf bf-cbc bf-cfb bf-ecb bf-ofb camellia-128-cbc camellia-128-ecb camellia-192-cbc camellia-192-ecb camellia-256-cbc camellia-256-ecb cast cast-cbc cast5-cbc cast5-cfb cast5-ecb cast5-ofb des des-cbc des-cfb des-ecb des-ede des-ede-cbc des-ede-cfb des-ede-ofb des-ede3 des-ede3-cbc des-ede3-cfb des-ede3-ofb des-ofb des3 desx rc2 rc2-40-cbc rc2-64-cbc rc2-cbc rc2-cfb rc2-ecb rc2-ofb rc4 rc4-40 seed seed-cbc seed-cfb seed-ecb seed-ofb".split(" ")
 
-for i in enc:
-	cmd = "openssl {} -d -in flag.encrypted -pass file:./password.txt".format(i[:-1])
-	out = commands.getoutput(cmd).split("\n")[0]
-	if out != "bad decrypt":
-		if re.match("SECCON",out):
-			print(out.split("\n")[0])
-			print("Decrypted!")
+for enc in Enc:
+    cmd = ["openssl", enc, "-d", "-in", "flag.encrypted", "-k", "53CC0NZOl6"]
+    out = subprocess.run(cmd,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+    try:
+        if "SECCON" in out.stdout.decode('utf8'):
+            print(out.stdout.decode('utf8'))
+    except:
+        pass
--- docs/Programming/x2.txt/solve.py
+++ docs/Programming/x2.txt/solve.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-f = open("x2.txt","rb")
-
-data = ""
-
-while 1:
-	b = f.read(1)
-	if b == "":
-		break
-	data += chr(ord(b)/2)
-print(data)
+d = open("x2.txt","rb").read()
+print("".join([chr(c//2) for c in d]))
